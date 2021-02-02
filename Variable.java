2d4c84e88ef982d2525294a8b1db78b6e26b440d
Prity <farzanaprity005@gmail.com>
2021-02-02 19:39:49 +1100

    #3 Variables and Data types (Problem 2) added

---
+++ Variable.java
@@ -0,0 +1,19 @@
+import javax.xml.crypto.Data;
+
+public class Variable {
+ //Write a program that will declare local, instance, and static variable and print them
+
+public int a=50;//instance variable  
+static int m=100;//static variable 
+public void printVar1() {
+    System.out.println("Instance Variable: " + a );}
+public static void main(String[] args){	 
+   
+    int i =90;//local variable  
+    //float a = i;
+    System.out.println("Local Variable " + i);
+    System.out.println("Static " + m);
+    Variable var1 = new Variable ();
+    var1.printVar1();
+
+}}
