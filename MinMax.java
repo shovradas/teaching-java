cecaa99e873693c58fe2d34ea18819df9699923e
Prity <farzanaprity005@gmail.com>
2021-02-03 00:18:36 +1100

    #3 Variables and Data types (Problem 2) added

---
+++ MinMax.java
@@ -0,0 +1,11 @@
+public class MinMax {
+        public static void main(String[] args) {     {
+     System.out.println(Integer.MIN_VALUE);
+     System.out.println(Integer.MAX_VALUE);
+     //System.out.println(Integer.MIN_VALUE - 1);
+     //System.out.println(Integer.MAX_VALUE + 1);      
+     System.out.println(Byte.MIN_VALUE); //extra
+     System.out.println(Byte.MAX_VALUE); // extra
+        }  
+        }  
+}
