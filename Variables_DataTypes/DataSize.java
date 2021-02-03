afe8d07efe4a497d630f26f3fdb97e8f07061a10
Prity <farzanaprity005@gmail.com>
2021-02-03 23:15:36 +1100

    #3 Variables and Primitive Data Types (DataSize) u

---
+++ Variables_DataTypes/DataSize.java
@@ -0,0 +1,19 @@
+package Variables_DataTypes;
+
+public class DataSize {
+    public static void main(String[] args) {
+
+        System.out.println(Double.SIZE);
+        System.out.println(Float.SIZE);
+        System.out.println(Long.SIZE);
+        System.out.println(Integer.SIZE);
+        System.out.println(Short.SIZE);
+        System.out.println(Byte.SIZE);
+
+        //System.out.println(Boolean.SIZE);//SIZE cannot be resolved or is not a fieldJava(33554502)
+        System.out.println(Character.SIZE);
+
+        
+    }
+    
+}
