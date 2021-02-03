713d5b3ff5a67bc61c93d881c3096285c5daa572
Prity <farzanaprity005@gmail.com>
2021-02-03 23:14:23 +1100

    #3 Variables and Primitive Data Types (DataType) u

---
+++ Variables_DataTypes/DataType.java
@@ -0,0 +1,24 @@
+package Variables_DataTypes;
+
+public class DataType {
+    public static void main(String[] args) {
+    double d = 12.0000000000000;
+    float f = 5.00000000f;
+    long l = 1211111111111l;
+    int i = 300000;
+    short s = 4536;
+    byte b = 122;
+
+    boolean bl = true;
+    char c = 'c';
+
+    System.out.println("Double : " + d);
+    System.out.println("Float : " + f);
+    System.out.println("Long : " + l);
+    System.out.println("Int : " + i);
+    System.out.println("Short : " + s);
+    System.out.println("Byte : " + b);
+    System.out.println("Boolean : " + bl);
+    System.out.println("Chatacter : " + c);
+}
+}
