eb8d15e9e41679c270d112cad3d43c6976af0fbd
Prity <farzanaprity005@gmail.com>
2021-02-14 09:38:43 +1100

    #10 issue updated

--- Loops/While/Number/bin_dec.java
+++ Loops/While/Number/bin_dec.java
@@ -8,17 +8,18 @@ public class bin_dec {
         int reminder;
         int dec = 0;
 
-        while (number>0) {
-        reminder = number%10;
-        if (i==1){ // for 2^0=1 when i is initiating with 1
-        power=power*i;}
-        else{
-            power=power*2; //from i++ = 2 and onwards power would be 2^i
-        }
-            dec = dec + (power*reminder);
-            number = number/10;
+        while (number > 0) {
+            reminder = number % 10;
+            if (i == 1) { // for 2^0=1 when i is initiating with 1
+                power = power * i;
+            } else {
+                power = power * 2; // from i++ = 2 and onwards power would be 2^i
+            }
+            dec = dec + (power * reminder);
+            number = number / 10;
             i++;
-            //System.out.println(dec);
-        }System.out.println(dec);
+            // System.out.println(dec);
+        }
+        System.out.println(dec);
     }
 }
--- Loops/While/Number/oct_dec.java
+++ Loops/While/Number/oct_dec.java
@@ -8,17 +8,18 @@ public class oct_dec {
         int reminder;
         int dec = 0;
 
-        while (n>0) {
-        reminder = n%10;
-        if (i==1){ 
-        power=power*i;}
-        else{
-            power=power*8;
-        }
-            dec = dec + (power*reminder);
-            n = n/10;
+        while (n > 0) {
+            reminder = n % 10;
+            if (i == 1) {
+                power = power * i;
+            } else {
+                power = power * 8;
+            }
+            dec = dec + (power * reminder);
+            n = n / 10;
             i++;
             System.out.println(dec);
-        }System.out.println(dec);
+        }
+        System.out.println(dec);
     }
 }
