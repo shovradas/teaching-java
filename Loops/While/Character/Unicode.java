95e6d564be765ca5427a35ec1d58c207daf51e1c
Prity <farzanaprity005@gmail.com>
2021-02-15 08:05:49 +1100

    #10 while loop updated

--- Loops/While/Character/Unicode.java
+++ Loops/While/Character/Unicode.java
@@ -2,14 +2,14 @@ package Loops.While.Character;
 
 public class Unicode {
     public static void main(String[] args) {
-        String unicL = "U+0980";
+        String unicL = "\u0980";
         char chL = unicL.charAt(0);
-        System.out.println(chL);
-        String unicU = "U+09EF";
+        //System.out.println(chL);
+        String unicU = "\u09EF";
         char chU = unicU.charAt(0);
         while (chL <= chU) {
             chL++;
-            // System.out.println(chL); //https://www.compart.com/en/unicode/block/U+0980
+             System.out.println(chL); //https://www.compart.com/en/unicode/block/U+0980
         }
     }
 }
